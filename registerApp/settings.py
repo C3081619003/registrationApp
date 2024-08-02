28bb0a3077d53be4dd9bebdd7f1268bb16213b44
b8021522 <b8021522@hallam.shu.ac.uk>
2024-08-02 15:53:55 +0100

    initial home

--- registerApp/settings.py
+++ registerApp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'studentApp.apps.StudentappConfig',
 ]
 
 MIDDLEWARE = [
